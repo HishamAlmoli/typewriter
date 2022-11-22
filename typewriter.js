f1c79cb918cb0e07692f25b27dfef00569f2b46d
HishamAlmoli <halmoli@my.centennialcollege.ca>
2022-11-22 17:36:24 -0500

    Second try

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,10 @@
 
 const typewriter = function() {
     const sentence = "hello there from lighthouse labs";
-    let counter =0;
+    let counterI =0;
     for (const char of sentence) {       
-        setTimeout(() => process.stdout.write(char), counter) // <= 1s delay to make it noticeable. Can dial it down later.
-        counter +=1000;
+        setTimeout(() => process.stdout.write(char), counterI) // <= 1s delay to make it noticeable. Can dial it down later.
+        counterI +=1000;
       }
 };
 typewriter();
